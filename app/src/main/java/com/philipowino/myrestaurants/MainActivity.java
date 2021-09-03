ba27baf797a7eefb4c8e720e466ef257da9eaa6b
Philip Owino <jofonowino@gmail.com>
2021-09-03 07:49:28 +0300

    add new activity and navigate using intent. Also add up button by adding meta- data in manifest for the created activity

--- app/src/main/java/com/philipowino/myrestaurants/MainActivity.java
+++ app/src/main/java/com/philipowino/myrestaurants/MainActivity.java
@@ -2,6 +2,7 @@ package com.philipowino.myrestaurants;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,9 +20,9 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Show a toast when the button is clicked.
-                Toast.makeText(MainActivity.this,"I'm Clicked !!", Toast.LENGTH_LONG)
-                        .show();
+                // Intent
+                Intent intent = new Intent(MainActivity.this, Restaurants.class);
+                startActivity(intent);
             }
         });
 
