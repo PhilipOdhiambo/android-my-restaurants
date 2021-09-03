331f03d3aa295eddad41ab6494bbb978f4cee84f
Philip Owino <jofonowino@gmail.com>
2021-09-03 13:50:33 +0300

    add @RunWith to ensure our code is run natively in JVM instead of on android

--- app/src/test/java/com/philipowino/myrestaurants/MainActivityTest.java
+++ app/src/test/java/com/philipowino/myrestaurants/MainActivityTest.java
@@ -1,4 +1,8 @@
 package com.philipowino.myrestaurants;
 
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
 }
