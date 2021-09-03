f7020b175b00e9b047a1bfce709d9fbc3a1c7f0b
Philip Owino <jofonowino@gmail.com>
2021-09-03 10:01:50 +0300

    add Toast Logct input text

--- app/src/main/java/com/philipowino/myrestaurants/MainActivity.java
+++ app/src/main/java/com/philipowino/myrestaurants/MainActivity.java
@@ -4,27 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button button;
+    private EditText location;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Custom code
+        // Initializations
         button = (Button) findViewById(R.id.button);
+        location = (EditText) findViewById(R.id.zipCode);
+
+        // Set on click listener for "find restaurant" button
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Intent
-                Intent intent = new Intent(MainActivity.this, Restaurants.class);
-                startActivity(intent);
+
+                String locationZip = location.getText().toString();
+
+                // Toast the value entered
+                Toast.makeText(MainActivity.this,locationZip,Toast.LENGTH_LONG).show();
+
+                // Log the value to Logcat
+                Log.d(TAG,locationZip);
             }
         });
+
+
+
+
 
     }
 }
