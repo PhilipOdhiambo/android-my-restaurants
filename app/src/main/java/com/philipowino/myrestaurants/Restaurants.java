41149c017aa3c4983a1538fd02a2b00270c14552
Philip Owino <jofonowino@gmail.com>
2021-09-04 23:37:05 +0300

    custom array adapters

--- app/src/main/java/com/philipowino/myrestaurants/Restaurants.java
+++ app/src/main/java/com/philipowino/myrestaurants/Restaurants.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,11 +20,8 @@ public class Restaurants extends AppCompatActivity {
     @BindView(R.id.locationView) TextView mLocation;
     @BindView(R.id.restaurantsLIstView) ListView mListView;
 
-    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
-            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
-            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
-            "Lardo", "Portland City Grill", "Fat Head's Brewery",
-            "Chipotle", "Subway"};
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro", "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil","Slappy Cakes", "Equinox", "Miss Delta's", "Andina", "Lardo", "Portland City Grill", "Fat Head's Brewery", "Chipotle", "Subway"};
+    private String[] cuisines = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +31,15 @@ public class Restaurants extends AppCompatActivity {
         ButterKnife.bind(this);
 
         // Initialize and set ArrayAdapter
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
-        mListView.setAdapter(arrayAdapter);
+        MyRestaurantsArrayAdapter adapter = new MyRestaurantsArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants, cuisines);
+        mListView.setAdapter(adapter);
 
         // Set OnItemClickListener to display Toast when listItem is clicked
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 String restaurant = ((TextView)view).getText().toString();
+                Log.v("RestaurantsActivity", "In the onItemClickListener!");
                 Toast.makeText(Restaurants.this, restaurant, Toast.LENGTH_LONG).show();
             }
         });
@@ -49,5 +48,6 @@ public class Restaurants extends AppCompatActivity {
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocation.setText("Here are all the restaurants near: " + location);
+        Log.d("RestaurantsActivity", "In the onCreate method!");
     }
 }
