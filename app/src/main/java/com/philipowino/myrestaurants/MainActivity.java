7998c37bf1c1011097d9aca6aa0910e06a67ae40
Philip Owino <jofonowino@gmail.com>
2021-09-05 00:20:11 +0300

    add View.OnClickListener Interface

--- app/src/main/java/com/philipowino/myrestaurants/MainActivity.java
+++ app/src/main/java/com/philipowino/myrestaurants/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
     public static final String TAG = MainActivity.class.getSimpleName();
 
     @BindView(R.id.button) Button button;
@@ -26,25 +26,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-
         // Set on click listener for "find restaurant" button
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        button.setOnClickListener(this);
+    }
 
-                // Passing data with Intent
-                String textToPass = location.getText().toString();
+    @Override
+    public void onClick(View view) {
 
-                Intent intent = new Intent(MainActivity.this,Restaurants.class);
-                intent.putExtra("location",textToPass);
-
-                startActivity(intent);
-            }
-        });
-
-
-
-
-
+        if (view == button) {
+            // Passing data with Intent
+            String textToPass = location.getText().toString();
+            Intent intent = new Intent(MainActivity.this,Restaurants.class);
+            intent.putExtra("location",textToPass);
+            startActivity(intent);
+        }
     }
 }
