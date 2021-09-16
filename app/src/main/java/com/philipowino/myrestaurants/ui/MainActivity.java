a23e1fb1c5495ecf587025784005be744a06ea5c
PhilipOdhiambo <jofonowino@gmail.com>
2021-09-16 10:03:24 +0300

    SearchVew Widget implemented

--- app/src/main/java/com/philipowino/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/philipowino/myrestaurants/ui/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,67 +23,33 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
-    public static final String TAG = MainActivity.class.getSimpleName();
-    private DatabaseReference mSearchedLocationReference;
-    private ValueEventListener mSearchedLocationReferenceListener;
-
     @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
-    @BindView(R.id.zipCode) EditText location;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
     @BindView(R.id.savedRestaurantsButton) Button mSavedRestaurantsButton;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mSearchedLocationReference = FirebaseDatabase
-                .getInstance()
-                .getReference()
-                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
-
-        // Add ValueEventListener
-        mSearchedLocationReferenceListener = mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) { ////something changed!
-                for (DataSnapshot locationSnapshot : snapshot.getChildren()) {
-                    String location = locationSnapshot.getValue().toString();
-                    Log.d("Locations updated", "location: " + location); // Log
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                //update UI here if error occurred.
-            }
-        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+
         mFindRestaurantsButton.setOnClickListener(this);
         mSavedRestaurantsButton.setOnClickListener(this);
-
     }
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View v) {
 
-        if (view == mFindRestaurantsButton) {
-            // Passing data with Intent
-            String textToPass = location.getText().toString();
-            mSearchedLocationReference.push().setValue(textToPass);
+        if(v == mFindRestaurantsButton) {
             Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
-            intent.putExtra("location",textToPass);
             startActivity(intent);
         }
-        if (view == mSavedRestaurantsButton) {
+
+        if (v == mSavedRestaurantsButton) {
             Intent intent = new Intent(MainActivity.this, SavedRestaurantListActivity.class);
             startActivity(intent);
         }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mSearchedLocationReference.removeEventListener(mSearchedLocationReferenceListener);
     }
 }
